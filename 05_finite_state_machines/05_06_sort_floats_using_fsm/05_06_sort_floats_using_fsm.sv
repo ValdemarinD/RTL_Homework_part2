f989a2296da0af5f95d81e75b32be80459dd2b7b
ValdemarinD <vept11.11.11@gmail.com>
2026-08-23 21:27:15 +0300

    update code

--- 05_finite_state_machines/05_06_sort_floats_using_fsm/05_06_sort_floats_using_fsm.sv
+++ 05_finite_state_machines/05_06_sort_floats_using_fsm/05_06_sort_floats_using_fsm.sv
@@ -20,6 +20,120 @@ module sort_floats_using_fsm (
     input                          f_le_res,
     input                          f_le_err
 );
+    
+    logic result0, result;
+    logic [2:0][FLEN - 1:0] buffer;
+
+    assign busy = (state != st_idle);
+
+
+    enum logic [2:0]
+    {
+        st_idle = 3'd0,
+        st_res1 = 3'd1,
+        st_res2 = 3'd2,
+        st_res3 = 3'd3,
+        st_ready = 3'd4,
+        st_err = 3'd5
+    }
+    state, next_state;
+
+    always_comb
+    begin
+        next_state = state;
+
+        case (state)
+        st_idle : if (valid_in) next_state = st_res1 ;
+        st_res1 : if ( f_le_err ) next_state = st_err;
+                  else next_state = st_res2 ;
+        st_res2 : if ( f_le_err ) next_state = st_err;
+                  else next_state = st_res3 ;
+        st_res3 : if ( f_le_err ) next_state = st_err;
+                  else next_state = st_ready;
+        st_ready : next_state = st_idle;
+        st_err : next_state = st_idle;
+        endcase
+    end
+
+    always_ff @ (posedge clk) begin
+        if (rst) begin
+            state <= st_idle;
+        end else
+            state <= next_state;
+    end
+
+    always_comb begin
+        f_le_a = '0;
+        f_le_b = '0;
+
+        case (state)
+            st_res1 : begin
+                        f_le_a = buffer[0];
+                        f_le_b = buffer[1];
+                      end
+            st_res2 : begin
+                        f_le_a = buffer[1];
+                        f_le_b = buffer[2];
+                      end   
+            st_res3 : begin
+                        f_le_a = buffer[0];
+                        f_le_b = buffer[1];
+                      end     
+        endcase
+    end
+
+    always_ff @ (posedge clk)
+    begin
+        if (rst) begin 
+            buffer <= '0;
+            sorted <= '0;
+            valid_out <= 1'b0;
+            err <= 1'b0;
+        end
+        else begin
+            valid_out <= 1'b0;
+            case (state)
+                st_idle : begin
+                            if (valid_in) begin
+                                buffer <= unsorted;
+                                err <= 1'b0;
+                            end
+                          end
+                st_res1 : begin
+                            if (f_le_err)
+                                err <= 1'b1;
+                           else if (!f_le_res) begin
+                               buffer[0] <= buffer[1];
+                               buffer[1] <= buffer[0];
+                           end
+                         end
+                st_res2 : begin
+                            if (f_le_err)
+                                err <= 1'b1;
+                            else if (!f_le_res) begin
+                                buffer[1] <= buffer[2];
+                                buffer[2] <= buffer[1];
+                            end
+                          end  
+                st_res3 : begin
+                            if (f_le_err)
+                                err <= 1'b1;
+                            else if (!f_le_res) begin
+                                buffer[0] <= buffer[1];
+                                buffer[1] <= buffer[0];
+                            end
+                          end
+                st_ready: begin 
+                    sorted <= buffer;
+                    valid_out <= 1'b1;
+                    end   
+                st_err : err <= 1'b1;
+            endcase
+        end
+    end
+
+
+    // The result
 
     // Task:
     // Implement a module that accepts three Floating-Point numbers and outputs them in the increasing order using FSM.
--- 05_finite_state_machines/05_07_float_discriminant/05_07_float_discriminant.sv
+++ 05_finite_state_machines/05_07_float_discriminant/05_07_float_discriminant.sv
@@ -19,6 +19,71 @@ module float_discriminant (
     output logic              busy
 );
 
+    logic error1, error2, error3, error4;
+    logic [FLEN - 1:0] preq_res_half_1;
+    logic [FLEN - 1:0] preq_res_half_2_1, preq_res_half_2_2;
+    logic [FLEN - 1:0] preq_res;
+    logic [FLEN - 1:0] a_r, b_r, c_r;
+    
+    f_mult i_mult_b
+    (
+        .a ( b_r ),
+        .b ( b_r ),
+        .res (preq_res_half_1),
+        .error (error1)
+    );
+
+    f_mult i_mult_ac
+    (
+        .a ( a_r ),
+        .b ( c_r ),
+        .res (preq_res_half_2_1),
+        .error (error2)
+    );
+
+    f_mult i_mult_4ac
+    (
+        .a ( preq_res_half_2_1 ),
+        .b (64'h4010_0000_0000_0000),
+        .res (preq_res_half_2_2),
+        .error (error3)
+    );
+
+    f_sub i_mult_B_4ac
+    (
+        .a ( preq_res_half_1 ),
+        .b ( preq_res_half_2_2 ),
+        .res (preq_res),
+        .error (error4)
+    );
+
+    always_ff @ (posedge clk) begin
+        if(rst) begin
+            a_r <= '0;
+            b_r <= '0;
+            c_r <= '0;
+            res_vld <= 1'b0;
+            res_negative <= 1'b0;
+            err <= 1'b0;
+            busy <= 1'b0;
+        end else begin
+            res_vld  <= 1'b0;
+            if(arg_vld && !busy) begin
+                busy <= 1'b1;
+                a_r <= a;
+                b_r <= b;
+                c_r <= c;
+                err <= 1'b0;
+            end else if(busy) begin
+                res <= preq_res;
+                res_negative <= preq_res[FLEN - 1];
+                err <= error1 | error2 | error3 | error4;
+                busy <= 1'b0;
+                res_vld <= 1'b1;
+            end else
+                busy <= 1'b0;    
+        end        
+    end 
     // Task:
     // Implement a module that accepts three Floating-Point numbers and outputs their discriminant.
     // The resulting value res should be calculated as a discriminant of the quadratic polynomial.
