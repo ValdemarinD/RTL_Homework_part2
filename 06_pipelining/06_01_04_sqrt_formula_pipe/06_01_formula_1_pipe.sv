7d75c4ad4f853b0d3e30884d6ed15f5c18b4e45a
ValdemarinD <vept11.11.11@gmail.com>
2026-08-24 13:03:15 +0300

    update code

--- 06_pipelining/06_01_04_sqrt_formula_pipe/06_01_formula_1_pipe.sv
+++ 06_pipelining/06_01_04_sqrt_formula_pipe/06_01_formula_1_pipe.sv
@@ -12,10 +12,84 @@ module formula_1_pipe
     input  [31:0] b,
     input  [31:0] c,
 
-    output        res_vld,
-    output [31:0] res
+    output logic   res_vld,
+    output logic [31:0] res
 );
 
+    logic        isqrt_1_x_vld;
+    logic        [31:0] isqrt_1_x;
+
+    logic        isqrt_1_y_vld;
+    logic        [15:0] isqrt_1_y;
+
+    logic        isqrt_2_x_vld;
+    logic        [31:0] isqrt_2_x;
+
+    logic        isqrt_2_y_vld;
+    logic        [15:0] isqrt_2_y;
+
+    logic        isqrt_3_x_vld;
+    logic        [31:0] isqrt_3_x;
+
+    logic        isqrt_3_y_vld;
+    logic        [15:0] isqrt_3_y;
+
+    logic [31:0] preq_res;
+
+   isqrt isqrt_1
+   (
+        .clk(clk),
+        .rst(rst),
+
+        .x_vld(isqrt_1_x_vld),
+        .x(isqrt_1_x),
+        .y_vld(isqrt_1_y_vld),
+        .y(isqrt_1_y)
+   );
+
+    isqrt isqrt_2
+   (
+        .clk(clk),
+        .rst(rst),
+
+        .x_vld(isqrt_2_x_vld),
+        .x(isqrt_2_x),
+        .y_vld(isqrt_2_y_vld),
+        .y(isqrt_2_y)
+   );
+
+    isqrt isqrt_3
+   (
+        .clk(clk),
+        .rst(rst),
+
+        .x_vld(isqrt_3_x_vld),
+        .x(isqrt_3_x),
+        .y_vld(isqrt_3_y_vld),
+        .y(isqrt_3_y)
+   );
+
+    assign isqrt_1_x_vld = arg_vld;
+    assign isqrt_2_x_vld = arg_vld;
+    assign isqrt_3_x_vld = arg_vld;
+
+    assign isqrt_1_x = a;
+    assign isqrt_2_x = b;
+    assign isqrt_3_x = c;
+
+
+    
+    always_ff @ (posedge clk) begin
+        if(rst) begin
+            res_vld <= 1'b0;
+            res <= '0;
+        end
+        else begin
+            if (isqrt_3_y_vld && isqrt_2_y_vld && isqrt_1_y_vld)
+                res <= {16'b0, isqrt_1_y} + {16'b0, isqrt_2_y} + {16'b0, isqrt_3_y};
+            res_vld <= isqrt_3_y_vld && isqrt_2_y_vld && isqrt_1_y_vld;
+        end
+    end
     // Task:
     //
     // Implement a pipelined module formula_1_pipe that computes the result
--- 06_pipelining/06_01_04_sqrt_formula_pipe/06_02_formula_1_pipe_aware_fsm.sv
+++ 06_pipelining/06_01_04_sqrt_formula_pipe/06_02_formula_1_pipe_aware_fsm.sv
@@ -24,6 +24,99 @@ module formula_1_pipe_aware_fsm
     input        [15:0] isqrt_y
 );
 
+    logic [31:0] buf_b;
+    logic [31:0] buf_c;
+    logic [31:0] sum_preq;
+    logic [1:0] counter;
+
+    enum logic [2:0]
+    {
+        st_idle       = 3'd0,
+        st_send_b     = 3'd1,
+        st_send_c     = 3'd2,
+        st_wait_a_res = 3'd3,
+        st_wait_b_res = 3'd4,
+        st_wait_c_res = 3'd5
+    }
+    state, next_state;
+
+    always_comb
+    begin
+        next_state = state;
+
+        case (state)
+        st_idle       : if ( arg_vld     ) next_state = st_send_b ;
+        st_send_b       : next_state = st_send_c ;
+        st_send_c       : next_state = st_wait_a_res ;
+        st_wait_a_res : if ( isqrt_y_vld ) next_state = st_wait_b_res ;
+        st_wait_b_res : if ( isqrt_y_vld ) next_state = st_wait_c_res ;
+        st_wait_c_res : if ( isqrt_y_vld ) next_state = st_idle;
+        endcase
+    end
+
+    always_ff @ (posedge clk) begin
+        if (rst) 
+            state <= st_idle;
+        else
+            state <= next_state;
+    end
+
+    always_ff @ (posedge clk) begin
+        if (rst) begin
+            buf_b <= '0;
+            buf_c <= '0;
+        end else if (arg_vld && state == st_idle) begin
+            buf_b <= b;
+            buf_c <= c;
+        end
+    end
+
+    always_comb
+    begin
+        isqrt_x_vld = 1'b0;
+        isqrt_x = 'x;
+
+        case (state)
+        st_idle :  begin
+            isqrt_x_vld = arg_vld;
+            isqrt_x = a;
+            end
+        st_send_b : begin
+            isqrt_x_vld <= 1'b1;
+            isqrt_x = buf_b;
+        end
+        st_send_c : begin 
+            isqrt_x_vld = 1'b1;
+            isqrt_x = buf_c;
+        end
+
+        endcase
+    end
+
+    always_ff @ (posedge clk)
+        if (rst) 
+            sum_preq <= '0;
+        else begin
+            if (state == st_idle && arg_vld)
+                sum_preq <= '0;
+            else if (isqrt_y_vld && state == st_wait_a_res)
+                sum_preq <= {16'b0, isqrt_y};
+            else if (isqrt_y_vld && state == st_wait_b_res)
+                sum_preq <= sum_preq + {16'b0, isqrt_y};
+        end
+
+    always_comb begin
+        res_vld = 1'b0;
+        res = sum_preq;
+        if(state == st_wait_c_res && isqrt_y_vld) begin
+            res_vld = 1'b1;
+            res = sum_preq + {16'b0, isqrt_y};
+        end
+    end
+
+
+
+
     // Task:
     //
     // Implement a module formula_1_pipe_aware_fsm
