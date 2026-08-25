1e5d3314c22a0a637aecb200fdbf766199a266eb
ValdemarinD <vept11.11.11@gmail.com>
2026-08-25 17:02:07 +0300

    update code

--- 07_fifo/07_07_sqrt_formula_pipe_using_fifo/07_07_formula_2_pipe_using_fifos.sv
+++ 07_fifo/07_07_sqrt_formula_pipe_using_fifo/07_07_formula_2_pipe_using_fifos.sv
@@ -15,6 +15,120 @@ module formula_2_pipe_using_fifos
     output        res_vld,
     output [31:0] res
 );
+    logic b_empty;
+    logic b_full;
+
+    logic a_empty;
+    logic a_full;
+
+    logic b_vld;
+    logic [31:0] b_del;
+
+    logic a_vld;
+    logic [31:0] a_del;
+
+    logic        isqrt_1_x_vld;
+    logic        [31:0] isqrt_1_x;
+
+    logic        isqrt_1_y_vld;
+    logic        [15:0] isqrt_1_y;
+
+    logic        isqrt_2_x_vld;
+    logic        [31:0] isqrt_2_x;
+
+    logic        isqrt_2_y_vld;
+    logic        [15:0] isqrt_2_y;
+
+    logic        isqrt_3_x_vld;
+    logic        [31:0] isqrt_3_x;
+
+    logic        isqrt_3_y_vld;
+    logic        [15:0] isqrt_3_y;
+
+    logic [31:0] preq_res;
+
+    
+
+   isqrt isqrt_1
+   (
+        .clk(clk),
+        .rst(rst),
+        .x_vld(arg_vld),
+        .x(c),
+        .y_vld(isqrt_1_y_vld),
+        .y(isqrt_1_y)
+   );
+
+flip_flop_fifo_with_counter 
+# (
+    .width(32),
+    .depth(16)
+)
+fifo_b
+(
+    .clk(clk),
+    .rst(rst),
+
+    .push(arg_vld),
+    .pop(isqrt_1_y_vld),
+
+    .write_data(b),
+    .read_data(b_del),
+
+    .empty(b_empty),
+    .full(b_full)
+);
+
+
+    assign isqrt_2_x_vld = isqrt_1_y_vld && !b_empty;
+    assign isqrt_2_x = b_del + {16'b0, isqrt_1_y};
+
+    isqrt isqrt_2
+   (
+        .clk(clk),
+        .rst(rst),
+        .x_vld(isqrt_2_x_vld),
+        .x(isqrt_2_x),
+        .y_vld(isqrt_2_y_vld),
+        .y(isqrt_2_y)
+   );
+
+    assign isqrt_3_x_vld = isqrt_2_y_vld && !a_empty;
+    assign isqrt_3_x = a_del + {16'b0, isqrt_2_y};
+
+flip_flop_fifo_with_counter 
+# (
+    .width(32),
+    .depth(33)
+)
+fifo_a
+(
+    .clk(clk),
+    .rst(rst),
+
+    .push(arg_vld),
+    .pop(isqrt_2_y_vld),
+
+    .write_data(a),
+    .read_data(a_del),
+
+    .empty(a_empty),
+    .full(a_full)
+);
+
+
+    isqrt isqrt_3
+   (
+        .clk(clk),
+        .rst(rst),
+        .x_vld(isqrt_3_x_vld),
+        .x(isqrt_3_x),
+        .y_vld(isqrt_3_y_vld),
+        .y(isqrt_3_y)
+   );
+
+    assign res_vld = isqrt_3_y_vld;
+    assign res = {16'b0, isqrt_3_y};
     // Task:
     //
     // Implement a pipelined module formula_2_pipe_using_fifos that computes the result
