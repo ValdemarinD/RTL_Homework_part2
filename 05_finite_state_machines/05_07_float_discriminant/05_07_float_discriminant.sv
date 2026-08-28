fae16821b34678ae33b1abd1131ec188b80c3e79
ValdemarinD <vept11.11.11@gmail.com>
2026-08-28 18:56:52 +0300

    update code

--- 05_finite_state_machines/05_07_float_discriminant/05_07_float_discriminant.sv
+++ 05_finite_state_machines/05_07_float_discriminant/05_07_float_discriminant.sv
@@ -23,67 +23,48 @@ module float_discriminant (
     logic [FLEN - 1:0] preq_res_half_1;
     logic [FLEN - 1:0] preq_res_half_2_1, preq_res_half_2_2;
     logic [FLEN - 1:0] preq_res;
-    logic [FLEN - 1:0] a_r, b_r, c_r;
     
     f_mult i_mult_b
     (
-        .a ( b_r ),
-        .b ( b_r ),
-        .res (preq_res_half_1),
-        .error (error1)
+        .a ( b ),
+        .b ( b ),
+        .res (preq_res_half_1)
     );
 
     f_mult i_mult_ac
     (
-        .a ( a_r ),
-        .b ( c_r ),
-        .res (preq_res_half_2_1),
-        .error (error2)
+        .a ( a ),
+        .b ( c ),
+        .res (preq_res_half_2_1)
     );
 
     f_mult i_mult_4ac
     (
         .a ( preq_res_half_2_1 ),
         .b (64'h4010_0000_0000_0000),
-        .res (preq_res_half_2_2),
-        .error (error3)
+        .res (preq_res_half_2_2)
     );
 
     f_sub i_mult_B_4ac
     (
         .a ( preq_res_half_1 ),
         .b ( preq_res_half_2_2 ),
-        .res (preq_res),
-        .error (error4)
+        .res (preq_res)
     );
 
-    always_ff @ (posedge clk) begin
-        if(rst) begin
-            a_r <= '0;
-            b_r <= '0;
-            c_r <= '0;
-            res_vld <= 1'b0;
-            res_negative <= 1'b0;
-            err <= 1'b0;
-            busy <= 1'b0;
-        end else begin
-            res_vld  <= 1'b0;
-            if(arg_vld && !busy) begin
-                busy <= 1'b1;
-                a_r <= a;
-                b_r <= b;
-                c_r <= c;
-                err <= 1'b0;
-            end else if(busy) begin
-                res <= preq_res;
-                res_negative <= preq_res[FLEN - 1];
-                err <= error1 | error2 | error3 | error4;
-                busy <= 1'b0;
-                res_vld <= 1'b1;
-            end else
-                busy <= 1'b0;    
-        end        
-    end 
+    function automatic logic is_invalid (input [FLEN - 1:0] val);
+        if (FLEN == 32)
+            return &val[30:23];
+        else
+            return &val[62:52];
+    endfunction
+
+    assign res_vld = arg_vld;
+    assign res = preq_res;
+    assign res_negative = preq_res[FLEN - 1];
+    assign err = is_invalid(a) | is_invalid(b) | is_invalid(c);
+    assign busy = 1'b0;
+
     // Task:
     // Implement a module that accepts three Floating-Point numbers and outputs their discriminant.
     // The resulting value res should be calculated as a discriminant of the quadratic polynomial.
